2f65c5dbed1f6be31fe875a01363343a10619bee
otto-dae <otto.acha.28@gmail.com>
2025-01-20 12:16:26 -0600

    First Activity Area-Volume

---
+++ MiniPractice/AreaVolume.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    system("clear");
+
+    const float PI = 3.14159;
+    float radio, area, volume;
+
+    cout << "Area and volume of a sphere in cms" << endl;
+    cout << "What is the radious?" << endl;
+
+    cin >> radio;
+
+    area = PI * pow(radio, 2);
+    cout << "The area of the circle is " << area << endl;
+
+    volume  = ((float)4 / (float)3) * PI * pow(radio,3);
+    cout << "The volume of the sphere is: " << volume << endl;
+
+    return 0;
+}
